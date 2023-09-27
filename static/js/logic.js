a561c9c1221d36a92b2cf5807bdf563f0eecb1f4
Prachi Shah <parikhprachi95@gmail.com>
2023-09-27 18:52:26 -0400

    Part1

---
+++ static/js/logic.js
@@ -0,0 +1,134 @@
+// Store our API endpoint as queryUrl
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl).then(function(earthquakeData) {
+    //Send data.features object to the createFeatures function.
+    console.log(earthquakeData);
+    createFeatures(earthquakeData.features);
+});
+// Create markers whose size increases with magnitude and color with depth
+function createMarker(feature, latlng) {
+    return L.circleMarker(latlng, {
+        radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.geometry.coordinates[2]),
+        color:"#000",
+        weight: 0.5,
+        opacity: 0.5,
+        fillOpacity: 1
+    });
+}
+
+function createFeatures(earthquakeData) {
+    //Define function to run for each feature in the features array.
+    // Give each feature a popup that describes the time and place of the earthquake.
+    function onEachFeature(feature, layer) {
+        layer.bindPopup(`<h3>Location:</h3> ${feature.properties.place}<h3> Magnitude:</h3> ${feature.properties.mag}<h3> Depth:</h3> ${feature.geometry.coordinates[2]}`);
+    }
+
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function for each piece of data in the array.
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: createMarker
+    });
+
+    // Send earthquakes layer to the createMap function
+    createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+    // Create the base layers
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      });
+    
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+      });
+
+    // Create a baseMaps object
+    let baseMaps = {
+        "Street Map": street,
+        "Topographic Map": topo
+    };
+    // Create map
+    let myMap = L.map("map", {
+        center: [37.09, -95.71],
+        zoom: 5,
+        layers: [street, earthquakes]
+    });
+
+     // Create an overlay object to hold our overlay.
+     let overlayMaps = {
+        "Earthquakes": earthquakes,
+    };
+
+    // Create a control
+    // Pass in baseMaps and overlayMaps
+    // Add the control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap); 
+    
+    function getColor(d) {
+         // variable to hold the color
+    let color = "";
+
+    if (d <= 1) {
+        return color = "#23613C";
+    }
+    else if (d <= 2) {
+        return color = "#408A5E";
+    }
+    else if (d <= 3) {
+        return color = "#77BC92";
+    }
+    else if (d <= 4) {
+        return color = "#E15C5C";
+    }
+    else if (d <= 5) {
+        return color = "#E63C3C";
+    }
+    else {
+        return color = "#FF0000";
+    }
+    }
+
+    let legend = L.control({position: 'bottomright'});
+
+    legend.onAdd = function (myMap) {
+
+        let div = L.DomUtil.create('div', 'info legend'),
+            grades = [-10, 10, 30, 60, 90],
+            labels = [],
+            legendInfo = "<h1>Magnitude</h1>";
+
+        for (let i = 0; i < grades.length; i++) {
+            div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '">&nbsp&nbsp&nbsp&nbsp</i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+        }    
+
+        return div;
+
+        };
+
+        // Add legend to map
+        legend.addTo(myMap);
+}
+
+// Increase marker size based on magnitude
+function markerSize(magnitude) {
+    return magnitude * 5;
+}
+
+// Change marker color based on depth
+function markerColor(depth) {
+    return depth > 90 ? '#FF0000' :
+            depth > 70 ? '#E63C3C' :
+            depth > 50 ? '#E15C5C' :
+            depth > 30 ? '#77BC92' :
+            depth > 10 ? '#408A5E' :
+                         '#23613C' ;          
+}
